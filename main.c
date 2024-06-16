37520e254fffc030f6aa4eba9adb78925d10436a
Jacob Patton <pattonjacob32@gmail.com>
2024-06-16 14:24:58 -0400

    add verbose actions

--- main.c
+++ main.c
@@ -4,21 +4,20 @@
 #include <stdbool.h>
 #include "./todos.h"
 
-enum ACTIONS {
+typedef enum ACTIONS {
 	CREATE,
 	LIST,
 	DELETE,
 	EDIT,
-	INVALID // this shouldn't happen if argument_guard and get_action are working
-};
+	HELP,
+	INVALID // this shouldn't happen if get_action is working
+} ACTIONS;
 
-void argument_guard(int argc, char *argv[]);
-enum ACTIONS get_action(char *argv[]);
+ACTIONS get_action(char *argv[], int argc);
+bool is_selected(char* arg, char* short_arg, char* verbose_arg);
 
 int main(int argc, char *argv[]) {
-	argument_guard(argc, argv);
-
-	enum ACTIONS action = get_action(argv);
+	ACTIONS action = get_action(argv, argc);
 
 	switch(action) {
 		case CREATE:
@@ -41,49 +40,29 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-void argument_guard(int argc, char *argv[]) {
-	if (argc == 1) {
-		printf("One argument is required.\n");
-		exit(1);
-	}
-	
-	// check if flag is valid
-	int FLAG_COUNTS = 4;
-	char* valid_flags[] = {"-c", "-l", "-d", "-e"};
-	bool is_valid_flag = false;
-	for (int i = 0; i < FLAG_COUNTS; i++) {
-		if (strcmp(argv[1], valid_flags[i]) == 0) {
-			is_valid_flag = true;
+ACTIONS get_action(char *argv[], int argc) {
+	if (argc >= 2) {
+		if (is_selected(argv[1], "-c", "--create")) {
+			return CREATE;
+		}
+		if (is_selected(argv[1], "-l", "--list")) {
+			return LIST;
+		}
+		if (is_selected(argv[1], "-d", "--delete")) {
+			return DELETE;
+		}
+		if (is_selected(argv[1], "-e", "--edit")) {
+			return EDIT;
+		}
+		if (is_selected(argv[1], "-e", "--edit")) {
+			return HELP;
 		}
 	}
-	if (is_valid_flag == false) {
-		printf("Flag argument is not valid.\nThe following flags are valid: ");
-		// list valid flags
-		for (int i = 0; i < FLAG_COUNTS; i++) {
-			printf("%s", valid_flags[i]);
-			if (i != FLAG_COUNTS - 1) {
-				printf(", ");
-			} else {
-				printf(".\n");
-			}
-		}
-		exit(1);
-	}
+	puts("Invalid action. Enter \"help\" for available actions.");
+	exit(1);
 }
 
-enum ACTIONS get_action(char *argv[]) {
-	if (strcmp(argv[1], "-c") == 0) {
-		return CREATE;
-	}
-	if (strcmp(argv[1], "-l") == 0) {
-		return LIST;
-	}
-	if (strcmp(argv[1], "-d") == 0) {
-		return DELETE;
-	}
-	if (strcmp(argv[1], "-e") == 0) {
-		return EDIT;
-	}
-	return INVALID;
+bool is_selected(char* arg, char* short_arg, char* verbose_arg) {
+	return strcmp(short_arg, arg) == 0 || strcmp(verbose_arg, arg) == 0;
 }
 
