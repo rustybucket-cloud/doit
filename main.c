c4c249ad71fed2153f1f7c9f2292bc6d022ec8ff
Jacob Patton <pattonjacob32@gmail.com>
2024-06-16 15:35:51 -0400

    add help menu

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ typedef enum ACTIONS {
 
 ACTIONS get_action(char *argv[], int argc);
 bool is_selected(char* arg, char* short_arg, char* verbose_arg);
+void list_help_options(void);
 
 int main(int argc, char *argv[]) {
 	ACTIONS action = get_action(argv, argc);
@@ -31,6 +32,9 @@ int main(int argc, char *argv[]) {
 			break;
 		case EDIT:
 			edit_todo(argv[2], argv[3]);
+			break;
+		case HELP:
+			list_help_options();
 			break;
 		default:
 			printf("Invalid action.\n");
@@ -54,7 +58,7 @@ ACTIONS get_action(char *argv[], int argc) {
 		if (is_selected(argv[1], "-e", "--edit")) {
 			return EDIT;
 		}
-		if (is_selected(argv[1], "-e", "--edit")) {
+		if (is_selected(argv[1], "-h", "--help")) {
 			return HELP;
 		}
 	}
@@ -66,3 +70,12 @@ bool is_selected(char* arg, char* short_arg, char* verbose_arg) {
 	return strcmp(short_arg, arg) == 0 || strcmp(verbose_arg, arg) == 0;
 }
 
+void list_help_options(void) {
+	puts("The following actions are available:");
+	puts("Action\t\tCommand\t\t\tDescription\t\tExample");
+	puts("------------------------------------------------------------------------------------");
+	puts("Create\t\t\"-c\" or \"--create\"\tCreates a new todo\tdoit -c \"walk the dog\"");
+	puts("List\t\t\"-l\" or \"--list\"\tList all created todos\tdoit -l");
+	puts("Delete\t\t\"-d\" or \"--delete\"\tDelete a todo by id\tdoit -d 1");
+	puts("Edit\t\t\"-e\" or \"--edit\"\tEdit a todo\t\tdoit -e 1 \"Walk the cat\"");
+}
